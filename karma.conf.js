e681fa02269ebab926a59e4b11cdec25a4751f3b
Alphe Salas <asalasm@correo.uss.cl>
2025-07-08 00:31:15 -0400

    mejora karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,33 @@
-coverageReporter: {
-  dir: require('path').join(__dirname, './coverage'),
-  subdir: '.',
-  reporters: [
-    { type: 'lcov', subdir: '.' },
-    { type: 'text-summary' }
-  ]
-}
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-coverage'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-spec-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // deja visible el resultado de Jasmine en el navegador
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'lcov', subdir: '.' }, // Necesario para Coveralls
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['spec', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadless'],
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
