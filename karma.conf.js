a491c85a88ac882b1d4e7571b0a6d15f94fdb8ba
Alphe Salas <asalasm@correo.uss.cl>
2025-07-08 00:48:27 -0400

    mejora karma.conf.js v2

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,6 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -5,29 +8,31 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-coverage'),
       require('karma-jasmine-html-reporter'),
-      require('karma-spec-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // deja visible el resultado de Jasmine en el navegador
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage'),
+      dir: require('path').join(__dirname, './coverage/asistmedic'),
       subdir: '.',
       reporters: [
-        { type: 'lcov', subdir: '.' }, // Necesario para Coveralls
+        { type: 'lcov', subdir: '.' }, // ← ¡Este es el que genera lcov.info!
         { type: 'text-summary' }
       ]
     },
-    reporters: ['spec', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['ChromeHeadless'],
-    singleRun: false,
+    reporters: ['progress', 'coverage'],
+    browsers: ['Chrome'],
     restartOnFileChange: true
   });
 };
